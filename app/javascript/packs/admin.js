59a98a1d1c79370301e269872aad683d870a7975
Franzé Jr <franzejr@gmail.com>
2017-06-19 19:05:07 -0300

    Start adding AdministerForm routes

---
+++ app/javascript/packs/admin.js
@@ -0,0 +1,46 @@
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'admin' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { AdministerForm, Api, Stores } from "formulae_react";
+const { Form } = Api;
+const { AdministerFormStore } = Stores;
+require("../../../node_modules/formulae_react/lib/css/index.css");
+
+// Just a thing to play with the form api
+// FIXME: Remove this eventually
+window.f = Form;
+
+document.onreadystatechange = function() {
+  var state = document.readyState;
+  if (state == "interactive") {
+    // nothing to do here lol
+  } else if (state == "complete") {
+    var adminElement = document.getElementById("administer-form");
+    if (adminElement) {
+      var formId = adminElement.getAttribute("data-form-id");
+      ReactDOM.render(
+        <Provider store={AdministerFormStore}>
+          <AdministerForm formId={formId} />
+        </Provider>,
+        adminElement
+      );
+    }
+
+    // Example for ViewFormSubmission
+    // ReactDOM.render(
+    //  <Provider store={ViewFormSubmissionStore}>
+    //    <ViewFormSubmission apiKey={1} formSubmissionId={1} />
+    //  </Provider>,
+    //  document.getElementById("view-form-submission")
+    //  );
+  }
+};
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'admin/forms/:id/edit' => 'admin#edit'
   root 'pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
