eedb614d19fc54b72949859edda3cab2ed2ea99d
Josh Adams <josh.rubyist@gmail.com>
2017-06-20 16:06:53 -0500

    Load the form from the API and hydrate the store

--- app/controllers/form_controller.rb
+++ app/controllers/form_controller.rb
@@ -2,7 +2,7 @@ class FormController < ApplicationController
   layout 'respond_to_form'
 
   def show
-    @form_id = 1
-    @form_display = params[:display] ? params[:display] : 'headings'
+    @form_id = params[:id]
+    @form_display = params[:display] ? params[:display] : 'HEADINGS'
   end
 end
